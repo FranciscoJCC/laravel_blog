ec2fde02dfeb07728cef597f3e8c412ea9d8dc1c
Francisco Cervantes <francisco@soft.law>
2022-05-06 16:25:34 -0500

    Se configuran migraciones y seeders de users y posts

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -14,7 +14,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => rand(1,10),
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->text(800),            
         ];
     }
 }
